932c617500c8000ec870fb7469b67c0c6c56a923
liuny <15989203850@163.com>
2024-12-02 16:55:50 +0800

    提交

---
+++ PTVersionSDK.xcframework/ios-arm64/PTVersionSDK.framework/Headers/PTVersionSDK.h
@@ -0,0 +1,18 @@
+//
+//  PTVersionSDK.h
+//  PTVersionSDK
+//
+//  Created by 刘宁银 on 2024/11/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PTVersionSDK.
+FOUNDATION_EXPORT double PTVersionSDKVersionNumber;
+
+//! Project version string for PTVersionSDK.
+FOUNDATION_EXPORT const unsigned char PTVersionSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PTVersionSDK/PublicHeader.h>
+
+
